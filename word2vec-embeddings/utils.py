2ae6b82ac87723fdc908496c6ee536e1a3c37711
agoyal3 <ankgoyal3@gmail.com>
2020-05-16 20:06:34 -0700

    Added the completed Negative Sampling excercise

--- word2vec-embeddings/utils.py
+++ word2vec-embeddings/utils.py
@@ -36,7 +36,7 @@ def create_lookup_tables(words):
     # # sorting the words from most to least frequent in text occurrence
     # sorted_vocab = sorted(word_counts, key=word_counts.get, reverse=True)
     # create int_to_vocab dictionaries
-    int_to_vocab = {ii: word for ii, word in enumerate(word_counts.most_common())}
+    int_to_vocab = {ii: word[0] for ii, word in enumerate(word_counts.most_common())}
     vocab_to_int = {word: ii for ii, word in int_to_vocab.items()}
 
     return vocab_to_int, int_to_vocab
